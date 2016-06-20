b950dfe30d9b5a19182747aa5e0475ef6166e13a
Emilio Almansi <ealmansi@gmail.com>
2016-06-20 18:08:21 -0300

    Multiples ejecuciones experimento con indice hashed.

--- parte3/log2csv/log2csv.py
+++ parte3/log2csv/log2csv.py
@@ -18,22 +18,25 @@ def main():
 
 
 def export(experimentId):
-  logFilePath = os.path.join(scriptDirectory, '../log/experiment-' + experimentId + '.log')
-  csvFilePath = os.path.join(scriptDirectory, '../csv/experiment-' + experimentId + '.csv')
-  documentCounts = []
-  with open(logFilePath ,'r') as logFile:
-    for line in logFile:
-      match = re.search("docs : ([0-9]+)", line)
-      if match:
-       documentCounts.append(match.group(1))
-  assert len(documentCounts) % numColumns == 0
-  with open(csvFilePath, 'wb') as csvFile:
-    csvWriter = csv.writer(csvFile)
-    for batchIndex in range(0, len(documentCounts) / numColumns):
-      first = batchIndex * numColumns
-      last = first + numColumns
-      csvRow = documentCounts[first : last]
-      csvWriter.writerow(csvRow)
+  for experimentNumber in range(1, 10 + 1):
+    logFilePath = os.path.join(scriptDirectory, '../log/experiment-' + experimentId + '-' + str(experimentNumber) + '.log')
+    csvFilePath = os.path.join(scriptDirectory, '../csv/experiment-' + experimentId + '-' + str(experimentNumber) + '.csv')
+    documentCounts = []
+    with open(logFilePath ,'r') as logFile:
+      for line in logFile:
+        match = re.search("docs : ([0-9]+)", line)
+        if match:
+         documentCounts.append(match.group(1))
+    assert len(documentCounts) % numColumns == 0
+    with open(csvFilePath, 'wb') as csvFile:
+      csvWriter = csv.writer(csvFile)
+      header = ["shard" + str(i) for i in range(1, numShards + 1)] + ['total']
+      csvWriter.writerow(header)
+      for batchIndex in range(0, len(documentCounts) / numColumns):
+        first = batchIndex * numColumns
+        last = first + numColumns
+        csvRow = documentCounts[first : last]
+        csvWriter.writerow(csvRow)
 
 
 if __name__ == '__main__':
